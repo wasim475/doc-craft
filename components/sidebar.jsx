57516cb45b3b944269a56c6c4b419f00d7a4ad77
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-04 15:35:22 +0600

    Update sidebar.jsx

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -1,3 +1,6 @@
+import Link from '../node_modules/next/link';
+
+
 export default function Sidebar({ docs }) {
   const roots = docs.filter((doc) => !doc.parent);
   const nonRoot = Object.groupBy(docs.filter((doc) => doc.parent),({ parent }) => parent);
@@ -10,45 +13,18 @@ export default function Sidebar({ docs }) {
 
       {
         roots.map((rootNode)=>(
-          <li key={rootNode.id}>{rootNode.title}</li>
+          <li key={rootNode.id} className="relative">
+          <Link
+            className="flex justify-between gap-2 py-1 pl-4 pr-3 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
+            href="/docs/quickstart"
+          >
+            <span className="truncate">{rootNode.title}</span>
+          </Link>
+        </li>
         ))
       }
 
-        <li className="relative">
-          <a
-            aria-current="page"
-            className="flex justify-between gap-2 py-1 pl-4 pr-3 text-sm text-zinc-900 transition dark:text-white"
-            href="/docs"
-          >
-            <span className="truncate">Introduction</span>
-          </a>
-          <ul role="list" style={{ opacity: 1 }}>
-            <li>
-              <a
-                className="flex justify-between gap-2 py-1 pl-7 pr-3 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-                href="/docs#guides"
-              >
-                <span className="truncate">Guides</span>
-              </a>
-            </li>
-            <li>
-              <a
-                className="flex justify-between gap-2 py-1 pl-7 pr-3 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-                href="/docs#resources"
-              >
-                <span className="truncate">Resources</span>
-              </a>
-            </li>
-            <li>
-              <a
-                className="flex justify-between gap-2 py-1 pl-7 pr-3 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-                href="/docs#test"
-              >
-                <span className="truncate">Test</span>
-              </a>
-            </li>
-          </ul>
-        </li>
+       
         {/* <li className="relative">
           <a
             className="flex justify-between gap-2 py-1 pl-4 pr-3 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
